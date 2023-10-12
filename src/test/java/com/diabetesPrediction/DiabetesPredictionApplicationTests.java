59e40dffba190842fd474045a90d5f1aa121bf3b
Madura Prasad <85684994+Madura-Prasad@users.noreply.github.com>
2023-10-12 19:57:16 +0530

    Project Created

---
+++ src/test/java/com/diabetesPrediction/DiabetesPredictionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diabetesPrediction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiabetesPredictionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
